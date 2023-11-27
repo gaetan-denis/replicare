6086440171bd329cf85efa505d92a6925c822aab
Gaëtan Denis <gaetandenis@outlook.be>
2023-11-27 11:52:18 +0100

    deleted registerTest.php, cause : wrong branch

---
+++ resources/views/register.blade.php
@@ -0,0 +1,19 @@
+<h1>Formulaire d’enregistrement</h1>
+<form action="" method="post" enctype="multipart/form-data">
+<label for="username">Votre nom d’utilisateur :</label>
+<input type="text"  id="username" name="username">
+<br>
+<label for="email">Votre email :</label>
+<input type="email" id="email" name="email">
+<br>
+<label for="password">Votre mot de passe :</label>
+<input type="password" id="password" name="password">
+<br>
+<label for="confirmPassword">Confirmez votre mot de passe :</label>
+<input type="password" id="confirmPassword" name="confirmPassword" accept="image/jpeg, image/jpg, image/png, image/gif, image/svg, image/webp">
+<br>
+<label for="avatar">Choisissez votre avatar :</label>
+<input type="file" id="avatar" name="avatar">
+<br>
+<input type="submit">
+</form>
