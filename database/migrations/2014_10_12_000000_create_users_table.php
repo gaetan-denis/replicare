106c9b822016a81a1935d3d0922fd0afeb75b976
Gaëtan Denis <gaetandenis@outlook.be>
2023-12-01 19:50:09 +0100

    created users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,11 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->id('users_id');
+            $table->string('users_username');
+            $table->string('users_email')->unique();
+            $table->string('users_password');
+            $table->string('users_avatar')->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
